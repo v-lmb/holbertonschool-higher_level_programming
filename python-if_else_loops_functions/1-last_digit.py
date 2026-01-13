bd417f413e0dbf673fc4d4abf3e19d3fc58db4b8
v-lmb <virginie.amamelia@gmail.com>
2026-01-13 21:24:07 +0100

    Fix: logical operators

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 num = number % 10
 num_neg = abs(number) % 10
-if number > 6:
+if number < 6:
     print(f"Last digit of {number} is {num} and is less than 6 and not 0")
-elif number < 5:
+elif number > 5:
     print(f"Last digit of {number} is -{num_neg} and is greater than 5")
 else:
     print(f"Last digit of {number} is {num} and is 0")
