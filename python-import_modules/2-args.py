72b0c718aa9730d4c511ebbe5689be6f317ae407
v-lmb <virginie.amamelia@gmail.com>
2026-01-15 22:11:39 +0100

    Fix: just 1 argv, re organize code, do 0 argv condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,13 +3,13 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        print(f"{len(argv) - 1} argument:".format(len(argv)))
-        print(f"{argv}: {argv}".format(len(argv)))
+        print(f"{len(argv) - 1} arguments.")
 
-    elif len(argv) > 1:
-        print(f"{len(argv) - 1} arguments:".format(len(argv)))
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+        print(f"1: {argv[1]}")
+
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
         for i in range(1, len(argv)):
-            print(f"{i}: {argv[i]}".format(argv))
-
-    else:
-        print(f"{len(argv)} arguments.".format(len(argv)))
+            print(f"{i}: {argv[i]}")
