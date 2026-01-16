e90bdab7a68c45b43fac3604f408f2591dc61e92
v-lmb <virginie.amamelia@gmail.com>
2026-01-16 17:03:21 +0100

    Add: .format()

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
         if num1 == 8 and num2 == 9:
-            print(f"{num1}{num2}")
+            print(f"{num1}{num2}".format(num1, num2))
         else:
-            print(f"{num1}{num2},", end=" ")
+            print(f"{num1}{num2},".format(num1, num2), end=" ")
