c9c383472a32ed994329811567e44e409841a35f
v-lmb <virginie.amamelia@gmail.com>
2026-01-19 10:46:10 +0100

    Fix: case: list = [1, 2, 3] / idx = 3

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx] < 0 or my_list[idx] > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    for inx in my_list[0:]:
+        if my_list[idx] < 0 or my_list[idx] > len(my_list):
+            return None
+        else:
+            return my_list[idx]
