345cfbb45a19cea6411fee71b96124a36591cf46
v-lmb <virginie.amamelia@gmail.com>
2026-01-21 21:17:48 +0100

    Fix: space at the end of line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        for element in line:
-            print("{:d}".format(element), end=' ')
-        print()
+        for i in range(len(line)):
+            if i == len(line) - 1:
+                print("{:d}".format(line[i]))
+            else:
+                print("{:d}".format(line[i]), end=' ')
