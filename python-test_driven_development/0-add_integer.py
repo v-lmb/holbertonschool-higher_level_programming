4865df3d7e1c94dbe811d975c8077c34073775ee
v-lmb <virginie.amamelia@gmail.com>
2026-01-22 21:02:48 +0100

    Remove: overflow error try, except etc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
-Python-test_driven_development.0-add_integer
+Add two integers or floats and return an integer
 """
 
+
 def add_integer(a, b=98):
-    """ Add two integers or floats and return an integer."""
+    """
+    Add two integers or floats and return an integer
+    Parametres
+    a : int or float
+    b : int or float
+    Return:
+    int
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    try:
-        a = int(a) or float(a)
-    except (OverflowError):
-        raise TypeError("a must be an integer")
-    try:
-        b = int(b) or float(b)
-    except (OverflowError):
-        raise TypeError("b must be an integer")
-
+    a = int(a)
+    b = int(b)
     return a + b
