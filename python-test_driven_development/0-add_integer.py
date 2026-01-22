eeb3937d76413ec6ffbe465c076897c9fc1900d2
v-lmb <virginie.amamelia@gmail.com>
2026-01-22 20:54:57 +0100

    Fix: overflow float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,15 +2,21 @@
 """
 Python-test_driven_development.0-add_integer
 """
+
 def add_integer(a, b=98):
-    """
-	Docstring pour add_integer
-	
-	:param a: int or float
-	:param b: int or float
-	"""
+    """ Add two integers or floats and return an integer."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    try:
+        a = int(a) or float(a)
+    except (OverflowError):
+        raise TypeError("a must be an integer")
+    try:
+        b = int(b) or float(b)
+    except (OverflowError):
+        raise TypeError("b must be an integer")
+
+    return a + b
