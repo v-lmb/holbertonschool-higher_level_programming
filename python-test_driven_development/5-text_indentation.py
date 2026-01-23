34fa2c4349fccc5df972fb5c79dd86fc9585f6da
v-lmb <virginie.amamelia@gmail.com>
2026-01-23 13:10:59 +0100

    Add: description to the code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+Add indentation
+"""
+
+
 def text_indentation(text):
+    """
+    Add indentation to a text next ., ? and :
+    Parametres
+    text: string
+    Return:
+    text with 2 new lines after each of these characters: ., ? and :
+    Raise:
+    TypeError
+    """
     new_text = text.replace('.', '.\n\n')
     new_text1 = new_text.replace('?', '?\n\n')
     new_text2 = new_text1.replace(':', ':\n\n')
