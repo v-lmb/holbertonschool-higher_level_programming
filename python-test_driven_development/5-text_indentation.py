00b1f3caddceb0a8891711019492f936c88178b5
v-lmb <virginie.amamelia@gmail.com>
2026-01-23 14:18:32 +0100

    Add: <BLANKLINE> to the doc, and add end="" to remove the extra line break

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -21,4 +21,4 @@ def text_indentation(text):
         if not isinstance(text, str):
             raise TypeError("text must be a string")
     finally:
-        print(new_text2)
+        print(new_text2, end="")
