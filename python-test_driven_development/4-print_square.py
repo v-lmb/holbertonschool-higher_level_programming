90e6cdef01081e8638b51af3a28e19f9badada2e
v-lmb <virginie.amamelia@gmail.com>
2026-01-23 14:56:18 +0100

    Fix: remove or to and for float 0

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size == 0:
         pass
-    if size is float or size < 0:
+    if size is float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
