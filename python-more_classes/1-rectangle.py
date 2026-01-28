7cee0152af76adc38316b09fc01d868d03e774d1
v-lmb <virginie.amamelia@gmail.com>
2026-01-28 11:29:02 +0100

    Add: more verifications & raises to def __init__

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,7 +14,15 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
