2c48da1d02f82b88857ff1674c71673b0c9eff62
v-lmb <virginie.amamelia@gmail.com>
2026-02-02 19:35:31 +0100

    Feat: class Rectangle with area and  __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Create a class
+"""
+
+
+class BaseGeometry:
+    """
+    BasGeometry class
+    Raise: Exception, TypeError, ValueError
+    """
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+        area
+        self: self
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer_validator
+        self: self
+        name: string
+        value: value
+        """
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class with parent BasGeometry
+    """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        if type(self.__width) is not int or type(self.__height) is not int:
+            raise TypeError(f"{self.name} must be an integer")
+        if self.__width <= 0 or self.__height <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+    def area(self):
+        """
+        area rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
