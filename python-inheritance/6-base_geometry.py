34de1c0799a50702c96dceea6dafe202b4f01949
v-lmb <virginie.amamelia@gmail.com>
2026-02-02 13:54:06 +0100

    Feat: a class BaseGeometry with area raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Create a class
+"""
+
+
+class BaseGeometry:
+    """
+    enpty class
+    Raise: Exception
+    """
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+        area
+        self: self
+        """
+        raise Exception("area() is not implemented")
