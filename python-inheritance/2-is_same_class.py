1f82425a699452b090bade145738b46c043c1c68
v-lmb <virginie.amamelia@gmail.com>
2026-02-02 13:52:26 +0100

    Feat: function that returns True if the object is exactly an instance of the specified class ; otherwise False

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that returns True
+if the object is exactly an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    class for Rectangle
+    obj: width of rectangle
+    a_class: height of rectangle
+    Return: True if is exactly the specified class
+    """
+    if type(obj) is a_class:
+        return True
