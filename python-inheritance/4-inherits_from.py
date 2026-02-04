969a7ff29fdd5eff9dd1ab33ee77a5afd73b5f02
v-lmb <virginie.amamelia@gmail.com>
2026-02-04 12:39:31 +0100

    Fix: commentary

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
 """
 
 
