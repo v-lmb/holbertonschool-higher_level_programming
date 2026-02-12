f9d0219cc808d6e116c6ce08ca91215f473cd05e
v-lmb <virginie.amamelia@gmail.com>
2026-02-12 16:58:48 +0100

    Fix: move list

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,10 +6,9 @@ import json
 def convert_csv_to_json(filename):
     try:
         with open(filename, "r") as f:
-            reader = csv.DictReader(f)
-            data = list(reader)
+            objt = csv.DictReader(f)
             with open("data.json", "w", encoding='utf-8') as file:
-                json.dump(data, file)
+                json.dump(list(objt), file)
             return True
     except FileExistsError:
         return False
