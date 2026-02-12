81b8edbc781f337f85e64ab72ee18db54e51e242
v-lmb <virginie.amamelia@gmail.com>
2026-02-12 16:55:26 +0100

    Fix: remove comments and change dump /load to dumps / loads

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,19 +5,14 @@ import json
 def serialize_and_save_to_file(data, filename):
     """
     serialize_and_save_to_file
-    data: A Python Dictionary with data
-    filename: The filename of the output JSON file.
-    If the output file already exists it should be replaced.
     """
     with open(filename, "w") as f:
-        f.write(json.dump(data))
+        f.write(json.dumps(data))
 
 
 def load_and_deserialize(filename):
     """
     load_and_deserialize
-    filename: The filename of the input JSON file This function returns
-    a Python Dictionary with the deseialized JSON data from the file.
     """
     with open(filename, "r") as f:
-        return json.load(f.read())
+        return json.loads(f.read())
