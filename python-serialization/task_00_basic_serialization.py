0ceb8b9a6bdfac3dd5326043dae9d378c404ff63
v-lmb <virginie.amamelia@gmail.com>
2026-02-12 15:59:19 +0100

    Feat: Basic Serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Basic Serialization
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    serialize_and_save_to_file
+    data: A Python Dictionary with data
+    filename: The filename of the output JSON file.
+    If the output file already exists it should be replaced.
+    """
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """
+    load_and_deserialize
+    filename: The filename of the input JSON file This function returns
+    a Python Dictionary with the deseialized JSON data from the file.
+    """
+    with open(filename, "r") as f:
+        json.load(f)
