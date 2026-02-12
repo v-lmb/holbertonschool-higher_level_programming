b8d10322f9341327d71cde3372d7a025bf2e6fd8
v-lmb <virginie.amamelia@gmail.com>
2026-02-12 16:49:11 +0100

    Add: encoding utf 8

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -11,7 +11,7 @@ def convert_csv_to_json(csv_filename):
         with open(csv_filename, "r") as f:
             reader = csv.DictReader(f)
             data = list(reader)
-            with open("data.json", "w") as file:
+            with open("data.json", "w", encoding='utf-8') as file:
                 json.dump(data, file)
             return True
     except FileExistsError:
