674a9779f55797c7fa8f3678fc60e7e75d33a210
v-lmb <virginie.amamelia@gmail.com>
2026-02-18 14:17:15 +0100

    Feat: Consuming and processing data from an API using Python

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,36 @@
+#!usr/bin/python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """
+    fetch_and_print_posts
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        print(f"Status Code: {response.status_code}")
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    """
+    fetch_and_save_posts
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        stuct_data = []
+        for post in posts:
+            stuct_data = {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+            stuct_data.append(stuct_data)
+    with open("post.csv", 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(stuct_data)
