07d754dfd367d0bd55f1859f0d06853fc5f7b989
v-lmb <virginie.amamelia@gmail.com>
2026-02-19 14:40:40 +0100

    Fix indentation with open

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,32 +4,29 @@ import csv
 
 
 def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     if response.status_code == 200:
         print(f"Status Code: {response.status_code}")
         posts = response.json()
-
         for post in posts:
             print(post['title'])
 
 
 def fetch_and_save_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     if response.status_code == 200:
         posts = response.json()
         stuct_data = []
-
         for post in posts:
             stuct_data.append({
                 'id': post['id'],
                 'title': post['title'],
                 'body': post['body']
             })
-
-    with open("posts.csv", 'w', newline='', encoding='utf-8') as f:
-        fieldnames = ['id', 'title', 'body']
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(stuct_data)
+        with open("posts.csv", 'w', newline='', encoding='utf-8') as f:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(stuct_data)
