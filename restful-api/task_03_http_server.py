eb16dfce624fcab838c1069cb30036307e9b0bbb
v-lmb <virginie.amamelia@gmail.com>
2026-02-19 14:15:40 +0100

    Fix: encode messages

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,7 +3,7 @@
 restful-api.task_03_http_server
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
+from json import dumps
 
 
 class BeautifulServer(BaseHTTPRequestHandler):
@@ -15,7 +15,7 @@ class BeautifulServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
+            self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
 
         elif self.path == '/data':
             data = {
@@ -26,28 +26,30 @@ class BeautifulServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            json_data = json.dumps(data)
+            json_data = dumps(data)
             self.wfile.write(json_data.encode('utf-8'))
 
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write("OK".encode('utf-8'))
 
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'apllication/json')
             self.end_headers()
-            info = {"version": "1.0", "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(info).encode('utf-8'))
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                }
+            self.wfile.write(dumps(info).encode('utf-8'))
 
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            error_msg = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_msg).encode('utf-8'))
+            self.wfile.write("Endpoint not found".encode('utf-8'))
 
 
 if __name__ == "__main__":
