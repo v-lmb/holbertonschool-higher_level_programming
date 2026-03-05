34959eaae94087b268b87cd28ad5bca548e71278
v-lmb <virginie.amamelia@gmail.com>
2026-03-05 17:38:41 +0100

    Fix: read csv as open file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,8 +7,9 @@ def convert_csv_to_json(filename):
     try:
         with open(filename, "r", newline='') as f:
             objt = csv.DictReader(f)
+            data = list(objt)
         with open("data.json", "w", encoding='utf-8') as file:
-            json.dump(list(objt), file)
+            json.dump(data, file)
         return True
     except FileExistsError:
         return False
