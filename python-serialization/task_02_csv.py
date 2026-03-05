605d61f5e4bcc93e79e5fd74ec51b734940dca56
v-lmb <virginie.amamelia@gmail.com>
2026-03-05 17:33:03 +0100

    Add: newline and fix indetation

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,10 +5,10 @@ import json
 
 def convert_csv_to_json(filename):
     try:
-        with open(filename, "r") as f:
+        with open(filename, "r", newline='') as f:
             objt = csv.DictReader(f)
-            with open("data.json", "w", encoding='utf-8') as file:
-                json.dump(list(objt), file)
-            return True
+        with open("data.json", "w", encoding='utf-8') as file:
+            json.dump(list(objt), file)
+        return True
     except FileExistsError:
         return False
