74a7a5f873b710224cf25c13f257206f610469ee
v-lmb <virginie.amamelia@gmail.com>
2026-03-19 23:56:18 +0100

    Add: Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// const lang = 'C is fun\nPython is cool\nJavaScript is amazing';
+// console.log(lang);
+// fonctionne aussi mais pas de loop... Mais fonctionne ! :D
+
+const tablang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const langue of tablang) {
+  console.log(langue);
+}
